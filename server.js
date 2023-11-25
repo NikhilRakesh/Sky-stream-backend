bdae16de4cf2aad965b45cb3c1085ec22026e3e9
Vijin <vfcvijin@gmail.com>
2023-11-25 23:47:06 +0530

    bug fixes m3u8

--- server.js
+++ server.js
@@ -5,149 +5,140 @@ import User from "./models/userModel.js";
 import Eadge from "./models/eadgeModel.js";
 import { streamKeys } from "./index.js";
 
+const nms = new NodeMediaServer({
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60,
+  },
+  http: {
+    port: 8000,
+    webroot: "./public",
+    mediaroot: "./media",
+    allow_origin: "*",
+  },
+  https: {
+    port: 8443,
+    key: "./privatekey.pem",
+    cert: "./certificate.pem",
+    allow_origin: "*",
+  },
+  trans: {
+    ffmpeg: "/usr/bin/ffmpeg",
+    tasks: [
+      {
+        app: "live",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        dashKeep: true,
+        mp4: true,
+        mp4Flags: "[movflags=frag_keyframe+empty_moov]",
+      },
+      {
+        app: "live6",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        dashKeep: true,
+        mp4: true,
+        mp4Flags: "[movflags=frag_keyframe+empty_moov]",
+      },
+      {
+        app: "live4",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        dashKeep: true,
+        mp4: true,
+        mp4Flags: "[movflags=frag_keyframe+empty_moov]",
+      },
+      {
+        app: "live8",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        dashKeep: true,
+        mp4: true,
+        mp4Flags: "[movflags=frag_keyframe+empty_moov]",
+      },
+      {
+        app: "live14",
+        hls: true,
+        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+        hlsKeep: true, // to prevent hls file delete after end the stream
+        dash: true,
+        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+        dashKeep: true,
+        mp4: true,
+        mp4Flags: "[movflags=frag_keyframe+empty_moov]",
+      },
+    ],
+  },
+  relay: {
+    ffmpeg: "/usr/bin/ffmpeg",
+    tasks: [],
+  },
+  auth: {
+    api: true,
+    api_user: "codenuity",
+    api_pass: "codenuity",
+  },
+});
 
-  const nms = new NodeMediaServer({
-    rtmp: {
-      port: 1935,
-      chunk_size: 60000,
-      gop_cache: true,
-      ping: 30,
-      ping_timeout: 60,
-    },
-    http: {
-      port: 8000,
-      webroot: "./public",
-      mediaroot: "./media",
-      allow_origin: "*",
-    },
-    https: {
-      port: 8443,
-      key: "./privatekey.pem",
-      cert: "./certificate.pem",
-      allow_origin: "*",
-    },
-    trans: {
-      ffmpeg: "/usr/bin/ffmpeg",
-      tasks: [
-        {
-          app: "live",
-          vc: "copy",
-          vcParam: [],
-          ac: "aac",
-          acParam: ["-ab", "64k", "-ac", "1", "-ar", "44100"],
-          rtmp: true,
-          rtmpApp: "live2",
-          hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-          dash: true,
-          dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-          mp4: true,
-          mp4Flags: "[movflags=frag_keyframe+empty_moov]",
-        },
-        {
-          app: "live6",
-          vc: "copy",
-          vcParam: [],
-          ac: "aac",
-          acParam: ["-ab", "64k", "-ac", "1", "-ar", "44100"],
-          rtmp: true,
-          rtmpApp: "live2",
-          hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-          dash: true,
-          dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-          mp4: true,
-          mp4Flags: "[movflags=frag_keyframe+empty_moov]",
-        },
-        {
-          app: "live4",
-          vc: "copy",
-          vcParam: [],
-          ac: "aac",
-          acParam: ["-ab", "64k", "-ac", "1", "-ar", "44100"],
-          rtmp: true,
-          rtmpApp: "live2",
-          hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-          dash: true,
-          dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-          mp4: true,
-          mp4Flags: "[movflags=frag_keyframe+empty_moov]",
-        },
-        {
-          app: "live8",
-          vc: "copy",
-          vcParam: [],
-          ac: "aac",
-          acParam: ["-ab", "64k", "-ac", "1", "-ar", "44100"],
-          rtmp: true,
-          rtmpApp: "live2",
-          hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-          dash: true,
-          dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-          mp4: true,
-          mp4Flags: "[movflags=frag_keyframe+empty_moov]",
-        },
-      ],
-    },
-    relay: {
-      ffmpeg: "/usr/bin/ffmpeg",
-      tasks: [],
-    },
-    auth: {
-      api: true,
-      api_user: "codenuity",
-      api_pass: "codenuity",
-    },
-  });
+nms.on("prePublish", async (id, StreamPath, args) => {
+  const isValidStreamKey = streamKeys.includes(StreamPath);
+  if (!isValidStreamKey) {
+    const session = nms.getSession(id);
+    return session.reject();
+  }
 
-  
-  nms.on("prePublish", async (id, StreamPath, args) => {
+  const channel = await Channel.findOne({ streamKey: StreamPath });
+  const user = await User.findById(channel.userId);
 
-      const isValidStreamKey = streamKeys.includes(StreamPath);
-      if (!isValidStreamKey) {
-        const session = nms.getSession(id);
-        return session.reject();
-      }
+  if (!user.isActive) {
+    const session = nms.getSession(id);
+    return session.reject();
+  }
 
-    const channel = await Channel.findOne({ streamKey: StreamPath });
-    const user = await User.findById(channel.userId);
+  await Channel.findOneAndUpdate(
+    { streamKey: StreamPath },
+    { $set: { status: true, startTime: new Date(Date.now()), sessionId: id } },
+    { new: true, multi: true }
+  );
 
-    if (!user.isActive) {
-      const session = nms.getSession(id);
-      return session.reject();
-    }
+  await User.findOneAndUpdate(
+    user._id,
+    { $set: { status: true } },
+    { new: true }
+  );
+});
 
-    await Channel.findOneAndUpdate(
-      { streamKey: StreamPath },
-      { $set: { status: true, startTime: new Date(Date.now()) ,sessionId:id} },
-      { new: true, multi: true }
-    );
+nms.on("donePublish", async (id, StreamPath, args) => {
+  const channel = await Channel.findOne({ streamKey: StreamPath });
+  const user = await User.findById(channel.userId);
 
-    await User.findOneAndUpdate(
-      user._id,
-      { $set: { status: true } },
-      { new: true }
-    );
-  });
- 
-  nms.on("donePublish", async (id, StreamPath, args) => {
-    const channel = await Channel.findOne({ streamKey: StreamPath });
-    const user = await User.findById(channel.userId);
+  await Channel.findOneAndUpdate(
+    { streamKey: StreamPath },
+    { $set: { status: false, startTime: null } },
+    { new: true, multi: true }
+  );
 
-    await Channel.findOneAndUpdate(
-      { streamKey: StreamPath },
-      { $set: { status: false, startTime: null } },
-      { new: true, multi: true }
-    );
+  await User.findOneAndUpdate(
+    user._id,
+    { $set: { status: false } },
+    { new: true }
+  );
+});
 
-    await User.findOneAndUpdate(
-      user._id,
-      { $set: { status: false } },
-      { new: true }
-    );
-  });
-
- 
-  export default nms;
+export default nms;
