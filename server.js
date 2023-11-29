72f9ecd4c663cf2b5ac787604423630425b235d3
Vijin <vfcvijin@gmail.com>
2023-11-30 01:33:04 +0530

    bug fix

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ import { streamKeys } from "./index.js";
         {
           app: "live",
           hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+          hlsFlags: "[hls_time=3:hls_list_size=6:hls_flags=delete_segments]",
           hlsKeep: false, // to prevent hls file delete after end the stream
           dash: true,
           dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
@@ -43,7 +43,7 @@ import { streamKeys } from "./index.js";
         {
           app: "live6",
           hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+          hlsFlags: "[hls_time=3:hls_list_size=6:hls_flags=delete_segments]",
           hlsKeep: true, // to prevent hls file delete after end the stream
           dash: true,
           dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
@@ -54,7 +54,7 @@ import { streamKeys } from "./index.js";
         {
           app: "live4",
           hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+          hlsFlags: "[hls_time=3:hls_list_size=6:hls_flags=delete_segments]",
           hlsKeep: true, // to prevent hls file delete after end the stream
           dash: true,
           dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
@@ -65,7 +65,7 @@ import { streamKeys } from "./index.js";
         {
           app: "live8",
           hls: true,
-          hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+          hlsFlags: "[hls_time=3:hls_list_size=6:hls_flags=delete_segments]",
           hlsKeep: true, // to prevent hls file delete after end the stream
           dash: true,
           dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
